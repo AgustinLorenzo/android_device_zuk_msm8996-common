4b9b9d2fb1e33f8159a98833c78cf9bcbc90156a
AgustinLorenzo <lineage.zukz2@gmail.com>
2018-05-05 22:42:01 +0200

    Link: https://github.com/Gabr0/device_leeco_msm8996-common-1/commit/0cb62b2194a77cc63b07365008f91407eaf27958

--- msm8996.mk
+++ msm8996.mk
@@ -417,4 +417,5 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service \
+    thermal.msm8996
