5db11ea7d9360d953f40b8e33a4fa3a043f6b86f
AgustinLorenzo <agustin.lorenzo@thinco.es>
2018-07-08 23:42:43 +0000

    Fallo

--- msm8996.mk
+++ msm8996.mk
@@ -415,5 +415,5 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
-    $(LOCAL_PATH)/configs/thermal-engine-vr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-vr.conf \
+    $(LOCAL_PATH)/configs/thermal-engine-vr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-vr.conf
 
